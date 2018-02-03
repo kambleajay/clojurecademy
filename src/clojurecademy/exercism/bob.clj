72642e0d28a0e512d43b42c12d7bd4a32c1a8208
Ajay Kamble <ajay.kamble@springernature.com>
2018-02-04 00:59:16 +0530

    completed solution for bob

--- src/clojurecademy/exercism/bob.clj
+++ src/clojurecademy/exercism/bob.clj
@@ -2,9 +2,33 @@
   "Solution for https://clojurecademy.com/course#!/subjects/17592186091322"
   (:require [clojure.string :as str]))
 
+(defn said-nothing?
+  [message]
+  (empty? (str/trim message)))
+
+(defn no-small-letters?
+  [message]
+  (empty? (re-seq #"[a-z]" message)))
+
+(defn some-capital-word?
+  [message]
+  (let [words (str/split message #" ")]
+    (some #(re-seq #"^[A-Z]+" %) words)))
+
+(defn yell?
+  [message]
+  (and
+   (no-small-letters? message)
+   (some-capital-word? message)))
+
+(defn question?
+  [s]
+  (str/ends-with? s "?"))
+
 (defn response-for
   [message]
-  (let [trimmed-message (str/trim message)]
-    (cond
-      (empty? trimmed-message) "Fine. Be that way!"
-      (str/ends-with? message "?") "Sure.")))
+  (cond
+    (said-nothing? message) "Fine. Be that way!"
+    (yell? message) "Woah, chill out!"
+    (question? message) "Sure."
+    :else "Whatever."))
--- test/clojurecademy/exercism/bob_test.clj
+++ test/clojurecademy/exercism/bob_test.clj
@@ -13,3 +13,21 @@
       (= "Sure." (response-for message))
     "Does this cryogenic chamber make me look fat?"
     "4?"))
+
+(deftest test-bob-says-whoa-chill-out-for-yells
+  (are [message]
+      (= "Woah, chill out!" (response-for message))
+    "WATCH OUT!"
+    "WHAT THE HELL WERE YOU THINKING?"
+    "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"
+    "1, 2, 3 GO!"
+    "I HATE YOU"))
+
+(deftest test-bob-says-whatever-to-anything-else
+  (are [message]
+      (= "Whatever." (response-for message))
+    "Tom-ay-to, tom-aaaah-to."
+    "Let's go make out behind the gym!"
+    "It's OK if you don't want to go to the DMV."
+    "Ending with ? means a question."
+    "1, 2, 3"))
